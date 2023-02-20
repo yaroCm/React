4a0deb27dd405f2d735ecc6a9baec4e25ab8267a
Jordi Cruz <jordi.cruz@ordermygear.com>
2023-02-20 11:39:07 -0600

    Google Login

--- 07-journal-app/src/firebase/providers.js
+++ 07-journal-app/src/firebase/providers.js
@@ -1,14 +1,24 @@
-import { GoogleAuthProvider } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { FirebaseAuth } from './config';
 
-const googleProvider = GoogleAuthProvider;
+const googleProvider = new GoogleAuthProvider();
 
-export const singInWithGoogle=async ()=>{
-    try{
-        const result =await singInWithPopup(FirebaseAuth,googleProvider);
-        const credentials = GoogleAuthProvider.credentialFromResult(result);
-        console.log('credentials', credentials);
-    }catch(error){
-        console.log('error', error);
-    }
-}
+export const singInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(FirebaseAuth, googleProvider);
+    const credentials = GoogleAuthProvider.credentialFromResult(result);
+    const { displayName, email, photoURL, uid } = result.user;
+    return {
+      ok: true,
+      displayName,
+      email,
+      photoURL,
+      uid,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      errorMessage: 'ERROR',
+    };
+  }
+};
--- 07-journal-app/src/store/auth/chekingGoogle.js
+++ 07-journal-app/src/store/auth/chekingGoogle.js
@@ -1,7 +1,12 @@
-import { checkingCredentials } from './authSlice';
+import { singInWithGoogle } from '../../firebase/providers';
+import { checkingCredentials, login, logout } from './authSlice';
 
 export const chekingGoogle = () => {
   return async (dispatch) => {
     dispatch(checkingCredentials());
+    const result = await singInWithGoogle();
+    console.log('result', result);
+    if (!result.ok) dispatch(logout(result.errorMessage));
+    dispatch(login(result));
   };
 };
