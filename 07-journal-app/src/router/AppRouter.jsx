258cf7733fb83808f7b115047bdecf90089a6244
Jordi <jordi.cruz@ordermygear.com>
2023-09-07 10:44:39 -0600

    Add checking auth

--- 07-journal-app/src/router/AppRouter.jsx
+++ 07-journal-app/src/router/AppRouter.jsx
@@ -2,8 +2,14 @@ import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { JournalPage } from '../journal/pages/JournalPage';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { CheckingAuth } from '../ui/components/CheckingAuth';
 
 export const AppRouter = () => {
+  // @ts-ignore
+  const { status } = useSelector((state) => state.auth);
+
+  if (status === 'checking') return <CheckingAuth />;
   return (
     <Routes>
       <Route path='/auth/*' element={<AuthRoutes />} />
---
+++ 07-journal-app/src/ui/components/CheckingAuth.jsx
@@ -0,0 +1,19 @@
+import { CircularProgress, Grid } from '@mui/material';
+import React from 'react';
+
+export const CheckingAuth = () => {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction='column'
+      alignItems='center'
+      justifyContent='center'
+      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+    >
+      <Grid item className='box-shadow' xs={3}>
+        <CircularProgress color='success' />
+      </Grid>
+    </Grid>
+  );
+};
